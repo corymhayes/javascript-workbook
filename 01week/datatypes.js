4e5d4d2be3cf6528949f27d2211e999adb4cca3f
Cory Hayes <Home@corys-air.attlocal.net>
2017-10-03 22:01:43 -0500

    datatypes.js in class

---
+++ 01week/datatypes.js
@@ -0,0 +1,46 @@
+/*
+
+Write a JavaScript program to display the current day and time.
+Write a JavaScript program to convert a number to a string.
+Write a JavaScript program to convert a string to the number.
+Write a JavaScript program that takes in different datatypes and prints out whether they are a:
+Boolean
+Null
+Undefined
+Number
+NaN
+String
+Write a JavaScript program that adds 2 numbers together.
+Write a JavaScript program that runs only when 2 things are true.
+Write a JavaScript program that runs when 1 of 2 things are true.
+Write a JavaScript program that runs when both things are not true.
+
+*/
+
+
+// Write a JavaScript program to display the current day and time
+// function that receives data for getDate and getTime object
+
+
+
+// Write a JavaScript program to convert a number to a string.
+// function takes in a number, toString method to make it a string, returns string
+
+
+
+// Write a JavaScript program to convert a string to the number.
+// function takes in the string, parseInt method to make it a number, return the integer
+
+
+
+/*
+Write a JavaScript program that takes in different datatypes and prints out whether they are a:
+Boolean
+Null
+Undefined
+Number
+NaN
+String
+*/
+
+// function takes in a argument, then use typeof to return what the data type is of said argument

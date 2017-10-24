84d83ce811c1d01229faa620d37f1d1cd4b1841b
Cory Hayes <corymhayes@gmail.com>
2017-10-24 18:42:41 -0500

    tictactoe beginning

--- 03week/ticTacToe.js
+++ 03week/ticTacToe.js
@@ -6,6 +6,7 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
 let board = [
   [' ', ' ', ' '],
   [' ', ' ', ' '],
@@ -13,6 +14,7 @@ let board = [
 ];
 
 let playerTurn = 'X';
+let winState = false;
 
 function printBoard() {
   console.log('   0  1  2');
@@ -24,23 +26,128 @@ function printBoard() {
 }
 
 function horizontalWin() {
-  // Your code here
+	/*
+
+		a horizontalWin is if playerTurn, X or 0 occupies board[0], board[1], board[2]
+		forEach(), return win
+
+	 */
+
+	board.map((val) => {
+		if(val.join('') === 'XXX' || val.join('') === 'OOO'){
+			winState = true;
+		}
+	});
+
+	return winState;
+
 }
 
-function verticalWin() {
-  // Your code here
+function verticalWin(colNum) {
+	/*
+
+		a verticalWin is if playerTurn occupies (0,0), (1,0), (2,0) // (0,1), (1,1), (2,1) // (0,2), (1,2), (2,2)
+		forEach(), return win
+
+	 */
+
+	let nArr = [];
+
+	board.map((val) => {
+		nArr.push(val[colNum]);
+	});
+	;
+
+
+	if (nArr.join('') === 'XXX' || nArr.join('') === 'OOO') {
+		winState = true;
+	}
+
+	return winState;
+
 }
 
 function diagonalWin() {
-  // Your code here
+	/*
+
+		a diagonalWin is if (0,2), (1,1), (2,0) // (0,0), (1,1), (2,2)
+		forEach(), return win
+
+	 */
+
+	const tlbrWin = () => {
+		const myArray = [];
+
+		board.map((val, index) => {
+			myArray.push(val[index]);
+	});
+
+		if(myArray.join('') === 'XXX' || myArray.join('') === 'OOO'){
+			winState = true;
+		}
+
+		return winState;
+	}
+
+	const trblWin = () => {
+		const myArray = [];
+
+		myArray.push(board[0][2]);
+		myArray.push(board[1][1]);
+		myArray.push(board[2][0]);
+
+		if(myArray.join('') === 'XXX' || myArray.join('') === 'OOO'){
+			winState = true;
+		}
+
+		return winState;
+	}
+
+	return tlbrWin();
+	return trblWin();
 }
 
 function checkForWin() {
-  // Your code here
+	/*
+
+		checks if any horizontalWin, verticalWin, and diagonalWin functions return playerTurn win
+
+	 */
+
+	horizontalWin();
+	diagonalWin();
+
+	if(winState === true){
+		console.log(`${playerTurn} wins`);
+		return true;
+	}
+
+
 }
 
 function ticTacToe(row, column) {
-  // Your code here
+  /*
+
+    check if the space is already occupied, if so re-prompt, else place playerTurn, X or O, into the space and change to the nextPlayer
+
+   */
+	checkForWin();
+	
+  if(board[row][column] === 'X' || board[row][column] === 'O') {
+  	console.log('Space already occupied');
+	  getPrompt();
+  } else {
+  	if(playerTurn === 'X') {
+		  board[row][column] = playerTurn;
+		  verticalWin(column);
+		  playerTurn = 'O';
+	  } else {
+		  board[row][column] = playerTurn;
+		  verticalWin(column);
+		  playerTurn = 'X';
+	  }
+  }
+
 }
 
 function getPrompt() {
@@ -72,7 +179,7 @@ if (typeof describe === 'function') {
     });
     it('should check for vertical wins', () => {
       board = [ [' ', 'X', ' '], [' ', 'X', ' '], [' ', 'X', ' '] ];
-      assert.equal(verticalWin(), true);
+      assert.equal(verticalWin(1), true);
     });
     it('should check for horizontal wins', () => {
       board = [ ['X', 'X', 'X'], [' ', ' ', ' '], [' ', ' ', ' '] ];
